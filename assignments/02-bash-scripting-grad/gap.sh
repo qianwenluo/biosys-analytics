c89612dca08a2538311e096fe6da2ab631cfdcd0
Qianwen Luo <qianwenluo@email.arizona.edu>
2019-01-29 09:34:33 -0700

    updated

--- assignments/02-bash-scripting-grad/gap.sh
+++ assignments/02-bash-scripting-grad/gap.sh
@@ -2,21 +2,32 @@
 
 set -u
 
-DIR="/rsgrps/bh_class/qianwenluo/biosys-analytics/data/gapminder"
-
+DIR="../../data/gapminder/"
 
 if [[ $# -eq 0 ]]; then
     i=0
     for FILE in `ls $DIR/* | sort -V`; do
-        ((i++))
+        let i++
         BASENAME=$(basename "$FILE")
-        echo "${BASENAME%.cc.txt}"
+        printf "%10d %s\n" "$((i))" "${BASENAME%.*.*}"
     done
+    exit 1
 fi
 
 ARG=$1
-find $DIR -type f -iname '$ARG*' -exec basename {} .cc.txt \;|grep \/ || printf "There are no countries starting with %s\n" \"$ARG\" 
 
+FILES=$(mktemp)
+find "$DIR" -type f -iname "$ARG*" |sort -V > "$FILES" 
+NUM_FILES=$(wc -l "$FILES" | awk '{print $1}')
 
+if [[ $NUM_FILES -lt 1 ]]; then
+    printf "There are no countries starting with %s\n" \"$ARG\"
+    exit 1
+fi
 
-
+i=0
+while read -r FILENAME; do
+    BASENAME=$(basename "$FILENAME")
+    printf "%10d %s\n" "$((i+1))" "${BASENAME%.cc.txt}"
+    i=$((i+1))
+done < "$FILES"
