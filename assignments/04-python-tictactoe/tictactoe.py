4fff752ef468d74629cebf0ae7a32ad6895b6e6c
Qianwen Luo <qianwenluo@email.arizona.edu>
2019-02-13 19:17:07 -0700

    grad homework

--- assignments/04-python-tictactoe/tictactoe.py
+++ assignments/04-python-tictactoe/tictactoe.py
@@ -20,7 +20,7 @@ def get_args():
         '-s',
         '--state',
         help='Board state',
-        metavar='int',
+        metavar='str',
         type=str,
         default=".........")
 
@@ -28,8 +28,9 @@ def get_args():
         '-p',
         '--player',
         help='Player',
-        metavar='int',
-        type=str)
+        metavar='str',
+        type=str,
+        default = '')
 
     parser.add_argument(
         '-c', 
@@ -37,6 +38,7 @@ def get_args():
         help='Cell to apply -p',
         metavar='int',
         type=int)
+
 
     return parser.parse_args()
 
@@ -74,17 +76,43 @@ def main():
         print('-------------')
         sys.exit(1)        
     
-    if not len(state_arg) == 9:
+    if state_arg and not len(state_arg) == 9:
         print('Invalid state "{}", must be 9 characters of only -, X, O'.format(state_arg)) 
         sys.exit(1)   
 
-    if not player_arg=='X' or 'O':
-        print('Invalid player "{}", must be X or O'.format(player_arg))    
-        sys.exit(1)
- 
-    if not 1<= cell_arg <=9:
-        print('Invalid cell "{}", must be 1-9'.format(cell_arg)) 
-        sys.exit(1)
+    if player_arg is not None and not player_arg in 'XO':
+        die('Invalid player "{}", must be X or O'.format(player_arg))    
+    
+    if cell_arg is not None and not 1 <= cell_arg <= 9:
+        die('Invalid cell "{}", must be 1-9'.format(cell_arg)) 
+     
+    if any([player_arg, cell_arg]) and not all ([player_arg, cell_arg]):
+        die('Must provide both --player and --cell')
+    
+    # print a valid state
+    state = list(state_arg)
+    for i, c in enumerate(state):
+        if c == '.':
+            state[i] = str(i+1)
+        if cell_arg is not None and player_arg in 'XO':
+            if state_arg[cell_arg-1] in 'XO':
+                die('Cell {} already taken'.format(cell_arg))
+            else:
+                state[cell_arg-1] = player_arg 
+    print('-------------')
+    print('|',state[0],'|',state[1],'|',state[2],'|')
+    print('-------------')
+    print('|',state[3],'|',state[4],'|',state[5],'|')
+    print('-------------')
+    print('|',state[6],'|',state[7],'|',state[8],'|')
+    print('-------------')        
+# move 
+    
+    #if board[cell_arg] == '':
+    #    board[cell_arg] = 
+    #else:
+    #    print('Cell {} already taken'.format(cell_arg))
+
 
 # --------------------------------------------------
 if __name__ == '__main__':
